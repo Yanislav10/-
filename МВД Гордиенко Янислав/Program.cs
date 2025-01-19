44fca34cd80ff072559e398790eaee319ed1c2da
XE <XE@XE4109>
2025-01-19 15:19:28 +0300

    добавлены коментарии

--- МВД Гордиенко Янислав/Program.cs
+++ МВД Гордиенко Янислав/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Типы данных в C#
+
+string variable_string = "эта строка";
+int variable_int = 12;
+double variable_double = 24.5;
+bool variable_bool = true;
+char variable_char = 'a';
+
+Console.WriteLine("тип данных - строчка " + variable_string);
+Console.WriteLine("тип данных - целочисленный " + variable_int);
+Console.WriteLine("тип данных - дробный " + variable_double);
+Console.WriteLine("тип данных - логический " + variable_bool);
+Console.WriteLine("тип данных - символьный " + variable_char);
+Console.WriteLine(variable_int + variable_double);
+
+// Сложение двух переменных в консоли для вывода
+
+Console.WriteLine("ввидите переменную типа int: ");
+int xInt;
+xInt = Int32.Parse(Console.ReadLine());
+Console.WriteLine(xInt + " - ваше значение. ");
+
+
+Console.WriteLine("ввидите переменную типа double: ");
+double xDouble;
+xDouble = Double.Parse(Console.ReadLine());
+Console.WriteLine(xDouble + " - ваше значение. ");
+
+Console.WriteLine("ввидите переменную типа bool: ");
+bool xBool;
+xBool = bool.Parse(Console.ReadLine());
+Console.WriteLine(xBool + " - ваше значение. ");
+
+Console.WriteLine("ввидите переменную типа char: ");
+char xChar;
+xChar = char.Parse(Console.ReadLine());
+Console.WriteLine(xChar + " - ваше значение. ");
+
+
